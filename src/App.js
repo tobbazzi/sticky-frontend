ce9cccf0342c157c4f44e41c9fe99321f1ffa344
tobbazzi <davide.bottazzi@yahoo.it>
2025-05-23 01:43:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://render.com/docs/web-services#port-binding');
 
 function App() {
   const [notes, setNotes] = useState([]);
